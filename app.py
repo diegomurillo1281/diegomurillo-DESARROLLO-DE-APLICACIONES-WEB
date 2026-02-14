fb9163e4564e3cfe2986e6c7fbe00ec1a561f8c6
DIEGO MURILLO <diegojaimemurillordenana@gmail.com>
2026-02-14 08:39:47 -0500

    Definiendo la variable app para Gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template
+
+# Esta es la línea que le falta a tu código
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Esto es lo que ya tienes al final
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
